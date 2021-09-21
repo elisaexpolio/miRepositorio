d261279a77cb1210d973df61658b8729cf0fd7cc
m <m@DESKTOP-LFLOFI2.home>
2021-09-21 19:07:34 +0200

    Elisa (21-09-21)(19:05)
    Enunciado otra vez

--- hoja1Ejercicio11/src/hoja1Ejercicio11/H1E11.java
+++ hoja1Ejercicio11/src/hoja1Ejercicio11/H1E11.java
@@ -1,9 +1,18 @@
 package hoja1Ejercicio11;
 
+import java.util.Scanner;
+
 public class H1E11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Elisa Expolio (21-09-21)
+		/*Ejercicio que hace un programa que lee nombre y año de nacimiento de una serie de alumnos hasta introducir
+		como nombre de alumno “fin”. En ese momento mostrará la edad media de los alumnos y
+		el nombre del alumno menor. NOTA: Al introducir como nombre “fin”, no nos ha de pedir el
+		año de nacimiento.*/
+		
+		
+		
 
 		
 	}
