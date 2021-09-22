44ac5b9f8d755dfb60c99843a39365b35e0a6d00
elisaexpolio <91023451+elisaexpolio@users.noreply.github.com>
2021-09-22 12:59:56 +0200

    OtroUsuario(hoja1Ejercicio11)(22-09-21)(12:59)
    Añado comentario

--- hoja1Ejercicio11/src/hoja1Ejercicio11/H1E11.java
+++ hoja1Ejercicio11/src/hoja1Ejercicio11/H1E11.java
@@ -7,28 +7,30 @@ public class H1E11 {
 
 	public static void main(String[] args) {
 		//Elisa Expolio (21-09-21)
-		/*Ejercicio que hace un programa que lee nombre y aÒo de nacimiento de una serie de alumnos hasta introducir
-		como nombre de alumno ìfinî. En ese momento mostrar· la edad media de los alumnos y
-		el nombre del alumno menor. NOTA: Al introducir como nombre ìfinî, no nos ha de pedir el
-		aÒo de nacimiento.*/
+		/*Ejercicio que hace un programa que lee nombre y a√±o de nacimiento de una serie de alumnos hasta introducir
+		como nombre de alumno ‚Äúfin‚Äù. En ese momento mostrar√° la edad media de los alumnos y
+		el nombre del alumno menor. NOTA: Al introducir como nombre ‚Äúfin‚Äù, no nos ha de pedir el
+		a√±o de nacimiento.*/
+		
+		//CAMBIO PARA PROBAR GITHUB
 		
 		//Declaramos las variables
 		String nombre;
-		int aÒoNacimiento;
+		int a√±oNacimiento;
 		float media;
-		int aÒoActual=Calendar.getInstance().get(Calendar.YEAR); //obtiene el aÒo del sistema (v·lido para todos los aÒos)
+		int a√±oActual=Calendar.getInstance().get(Calendar.YEAR); //obtiene el a√±o del sistema (v√°lido para todos los a√±os)
 		int edad;
 		int suma=0; //contador para las edades
 		int numNombres=0; //contador para el numero de nombres introducido
-		//boolean continuar=true; activar para SOLUCI”N 1
+		//boolean continuar=true; activar para SOLUCI√ìN 1
 		int menor=Integer.MAX_VALUE; //para sacar el nombre del alumno de menor edad
-		String nombreMenor = ""; //variable para guardar el nombre del alumno m·s pequeÒo. Inicializamos asÌ para que no de error por los if
+		String nombreMenor = ""; //variable para guardar el nombre del alumno m√°s peque√±o. Inicializamos as√≠ para que no de error por los if
 								 //tambien lo podemos hacer con null
 		String mensajeMedia;
 		String mensajeMenor;
 		Scanner sc=new Scanner(System.in);
 		
-		/*SOLUCI”N 1:
+		/*SOLUCI√ìN 1:
 		//Como no sabemos el numero de iteraciones es un bucle do-while
 		do
 		{
@@ -36,7 +38,7 @@ public class H1E11 {
 			System.out.print("Introduzca nombre: ");
 			nombre=sc.nextLine();
 			
-			//solo pedimos el aÒo al usuario si el nombre es distinto de fin
+			//solo pedimos el a√±o al usuario si el nombre es distinto de fin
 			if (nombre.equalsIgnoreCase("fin"))  //ignore case se refiere a que puedo escribir fin con mayusculas
 												 //o con mezcla mayusculas y minusculas que va a funcionar igual
 			{
@@ -47,11 +49,11 @@ public class H1E11 {
 				//sumamos 1 al contador del numero de nombres
 				numNombres++; //si lo ponemos despues del nombre estariamos sumando uno mas
 				
-				System.out.print("Introduzca aÒo de nacimiento: ");
-				aÒoNacimiento=Integer.valueOf(sc.nextLine()); //øpor quÈ? (ver apuntes)
+				System.out.print("Introduzca a√±o de nacimiento: ");
+				a√±oNacimiento=Integer.valueOf(sc.nextLine()); //¬øpor qu√©? (ver apuntes)
 				
-				//calculamos la edad haciendo la resta con el aÒo actual
-				edad=aÒoActual-aÒoNacimiento;
+				//calculamos la edad haciendo la resta con el a√±o actual
+				edad=a√±oActual-a√±oNacimiento;
 				
 				//acumulamos la edad en la suma para luego hacer la media
 				suma=suma+edad;
@@ -87,7 +89,7 @@ public class H1E11 {
 		System.out.println(mensajeMedia);
 		System.out.println(mensajeMenor);*/
 		
-		//SOLUCI”N 2:
+		//SOLUCI√ìN 2:
 		//Pedimos el nombre
 		System.out.print("Introduzca nombre: ");
 		nombre=sc.nextLine();
@@ -98,11 +100,11 @@ public class H1E11 {
 			//sumamos 1 al contador del numero de nombres
 			numNombres++; //si lo ponemos despues del nombre estariamos sumando uno mas
 			
-			System.out.print("Introduzca aÒo de nacimiento: ");
-			aÒoNacimiento=Integer.valueOf(sc.nextLine()); //øpor quÈ? (ver apuntes)
+			System.out.print("Introduzca a√±o de nacimiento: ");
+			a√±oNacimiento=Integer.valueOf(sc.nextLine()); //¬øpor qu√©? (ver apuntes)
 			
-			//calculamos la edad haciendo la resta con el aÒo actual
-			edad=aÒoActual-aÒoNacimiento;
+			//calculamos la edad haciendo la resta con el a√±o actual
+			edad=a√±oActual-a√±oNacimiento;
 			
 			//acumulamos la edad en la suma para luego hacer la media
 			suma=suma+edad;
