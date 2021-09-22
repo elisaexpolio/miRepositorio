7c13354c799d7a12945968fa4dd1192adf8eb03e
m <elisaexpoliorioja@gmail.com>
2021-09-22 18:01:34 +0200

    Elisa (22-09-21)(17:16)
    Ejercicio 12 resuelto!

--- hoja1Ejercicio12/src/hoja1Ejercicio12/H1E12.java
+++ hoja1Ejercicio12/src/hoja1Ejercicio12/H1E12.java
@@ -1,6 +1,6 @@
 package hoja1Ejercicio12;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class H1E12 {
 
@@ -9,47 +9,45 @@ public class H1E12 {
 		/*Ejercicio que devuelve los numeros primos entre 1 y 100*/
 		
 		//Declaramos las variables
-		int numero;
 		int resto; 
-		boolean primo=true;  //asumimos primo de inicio
-		String mensaje;
-		Scanner sc=new Scanner(System.in);
+		boolean primo;  //no podemos inicializarlo aquí porque sino el bucle deja de hacerse en cuanto
+						//detecta el primer número no primo.
+		//String mensaje;
+		//Scanner sc=new Scanner(System.in);
 		
-		//Pedimos un numero
-		System.out.print("Introduzca un numero: ");
-		numero=sc.nextInt();
-		
-		sc.close();
-		
-		//Quitamos el 0,1 y los negativos 
-		if(numero<2)
+		//Sacamos el número a través del un bucle for, ya no lo pedimos por teclado e introducimos 
+		//el código de los primos
+		for (int numero=1; numero<=100; numero++)
 		{
-			primo=false;
+			//Quitamos el 0,1 y los negativos 
+			if(numero<2)
+			{primo=false;} //para negativos, 0 y 1 siempre va a ser faslo
+			else
+			{primo=true;} //la inicializamos para el resto de números como verdadera y luego ya vemos 
+						  //si cambia con el bucle de los divisores, pero de inicio es verdadera.
+			
+			//Creamos un bucle for
+			for (int candidatoDivisor=2; candidatoDivisor<numero && primo==true; candidatoDivisor++)
+			//el bucle excluye el 1 y el propio numero como divisores (condicion de primo, siempre se cumple)
+			//por tanto si son divisibles entre otro número ya no serán primos
+			{
+				//calculamos el resto
+				resto=numero%candidatoDivisor;
+					
+				//si el resto es cero, hemos encontrado un divisor
+				if (resto==0)
+				{primo=false;}
+					
+			}
+			
+			//Sacamos por pantalla, tiene que sacar cada numero, por tanto dentro del for
+			if (primo==true)
+			{System.out.println(numero);}
 		}
 		
-		//Creamos un bucle for
-		for (int candidatoDivisor=2; candidatoDivisor<numero && primo==true; candidatoDivisor++)
-		//el bucle excluye el 1 y el propio numero como divisores (condicion de primo)
-		//por tanto si otro numero
-		{
-			//calculamos el resto
-			resto=numero%candidatoDivisor;
-				
-			//si el resto es cero, hemos encontrado un divisor
-			if (resto==0)
-			{primo=false;}
-				
-		}
-			
-		
-		//Sacamos el mensaje
-		mensaje=(primo==true)?"El número " + numero + " es primo.":"El número " + numero + " NO es primo";
-		/*if(primo==true)
-		{mensaje="El número " + numero + " es primo.";}
-		else
-		{mensaje="El número " + numero + " NO es primo";}*/
-			
-		System.out.println(mensaje);
+		//Tenemos que inicializarla como verdadera cada vez que se pruebe un número nuevo del 1 al 100
+		//Por eso se inicializa dentro del primer bucle for para que se re-inicialice después comprobar
+		//cada número
 
 	}
 
